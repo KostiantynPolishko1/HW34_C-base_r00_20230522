0e263d5b682715f4b57e7f11a0630bd13d3217c1
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-05-30 17:41:59 +0300

    5 test

--- main_HW34.cpp
+++ main_HW34.cpp
@@ -1,14 +1,9 @@
 ﻿#include<iostream>
+#include<vector>
 using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	//system("chcp_1251");
-
-	cout << "проверка вывода кириллицы" << endl;
-	cout << "кодировка UTF-8" << endl;
-	cout << "кодировка UTF-8 со спецификацией" << endl;
 
 	return 0;
 }
