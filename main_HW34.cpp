9d7e24fa84c1b86c8f6303a08ff75e252183724a
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-05-30 05:55:55 +0300

    4 deactivated system("chcp_1251")

--- main_HW34.cpp
+++ main_HW34.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	system("chcp_1251");
+	//system("chcp_1251");
 
 	cout << "проверка вывода кириллицы" << endl;
 	cout << "кодировка UTF-8" << endl;
+	cout << "кодировка UTF-8 со спецификацией" << endl;
 
 	return 0;
 }
